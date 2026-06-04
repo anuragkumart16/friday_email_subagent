25b578dacf0943a175ee522e54a375db3412dd1a
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-06-04 21:24:39 +0530

    test : trying to isolate deployment issue

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,6 @@
-// import app from "./../src/app"
+import app from "./../src/app"
 
-// export default app 
+export default app 
 
 
-// // this file lets the server work on vercel.
-
-import { StateGraph } from "@langchain/langgraph";
-
-console.log("Before graph");
-
-const graph = new StateGraph({} as any);
-
-console.log("After graph");
-
-export default function handler(req: any, res: any) {
-  return res.json({ success: true });
-}
+// this file lets the server work on vercel.
