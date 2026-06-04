6db9bbcd9b03028d4b2290216a71101bc239e16d
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-06-04 20:56:42 +0530

    test : trying to isolate deployment issue

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,13 @@
-import app from "./../src/app"
+// import app from "./../src/app"
 
-export default app 
+// export default app 
 
 
-// this file lets the server work on vercel.
+// // this file lets the server work on vercel.
 
+export default function handler(req: any, res: any) {
+  return res.status(200).json({
+    success: true,
+    message: "Hello from Vercel",
+  });
+}
