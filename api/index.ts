2586ee3ca18ac0ab8c9381432700f2c06606962a
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-06-04 21:00:06 +0530

    test : trying to isolate deployment issue

--- api/index.ts
+++ api/index.ts
@@ -5,18 +5,17 @@
 
 // // this file lets the server work on vercel.
 
-// export default function handler(req: any, res: any) {
-//   return res.status(200).json({
-//     success: true,
-//     message: "Hello from Vercel",
-//   });
-// }
-
-
 import { StateGraph } from "@langchain/langgraph";
+import { Annotation } from "@langchain/langgraph";
+
+const TestState = Annotation.Root({
+  message: Annotation<string>(),
+});
+
+const graph = new StateGraph(TestState);
 
 export default function handler(req: any, res: any) {
   return res.status(200).json({
-    success: true
+    success: true,
   });
 }
